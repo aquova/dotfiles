f161f0adb4c5783e8b372f9fd5e41e42811989a5
aquova <mail@aquova.net>
2024-11-14 12:59:21 -0600

    Add note about navbuddy

--- nvim/init.lua
+++ nvim/init.lua
@@ -31,6 +31,7 @@ require("lazy").setup({
         'lewis6991/gitsigns.nvim', -- gitsigns also used outside of barbar
     }},
     {'sindrets/diffview.nvim', cond = not vim.g.vscode},
+    -- Note: Navbuddy only works if language LSP is installed
     {'SmiteshP/nvim-navbuddy', cond = not vim.g.vscode, opts = {lsp = {auto_attach = true}}, dependencies = {
             "SmiteshP/nvim-navic",
             "MunifTanjim/nui.nvim",
