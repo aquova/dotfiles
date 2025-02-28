e2a8820174d3435dd0e6871e83bfbfa1265ef657
aquova <mail@aquova.net>
2025-02-28 14:20:52 -0600

    Update neovim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
     {'kyazdani42/nvim-tree.lua', cond = not vim.g.vscode, dependencies = {
         'kyazdani42/nvim-web-devicons',
     }},
+    {'lewis6991/gitsigns.nvim'},
     {'linux-cultist/venv-selector.nvim', branch = "regexp", cond = not vim.g.vscode},
     {'lukas-reineke/indent-blankline.nvim', cond = not vim.g.vscode},
     {'mbbill/undotree', cond = not vim.g.vscode},
@@ -24,18 +25,20 @@ require("lazy").setup({
     {'nvim-telescope/telescope.nvim', cond = not vim.g.vscode, dependencies = {
         'nvim-lua/plenary.nvim',
     }},
-    {'nvim-treesitter/nvim-treesitter', cond = not vim.g.vscode},
+    {'nvim-treesitter/nvim-treesitter', cond = not vim.g.vscode, dependencies = {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+    }},
     {'romgrk/barbar.nvim', cond = not vim.g.vscode, dependencies = {
         'kyazdani42/nvim-web-devicons',
-        'lewis6991/gitsigns.nvim', -- gitsigns also used outside of barbar
     }},
     {'sindrets/diffview.nvim', cond = not vim.g.vscode},
     {'SmiteshP/nvim-navbuddy', cond = not vim.g.vscode, opts = {lsp = {auto_attach = true}}, dependencies = {
-            "SmiteshP/nvim-navic",
-            "MunifTanjim/nui.nvim",
+        'SmiteshP/nvim-navic',
+        'MunifTanjim/nui.nvim',
     }},
     {'smoka7/hop.nvim'},
     {'stevearc/oil.nvim', cond = not vim.g.vscode},
+    {'toppair/peek.nvim', cond = not vim.g.vscode, event = "VeryLazy", build = "deno task --quiet build:fast"},
     {'tpope/vim-commentary'},
     {'tpope/vim-repeat'},
     {'tpope/vim-surround'},
@@ -55,7 +58,12 @@ require("hop").setup()
 if not vim.g.vscode then
     require("diffview").setup()
     require("gitsigns").setup()
+    require("ibl").setup()
+    require("marks").setup()
     require("nvim-tree").setup()
+    require('oil').setup()
+    require('onedark').load()
+    require("venv-selector").setup()
 
     require("barbar").setup{
         insert_at_end = true,
@@ -90,7 +98,6 @@ if not vim.g.vscode then
         }
     })
 
-    require("ibl").setup()
 
     local lsp = require("lsp-zero")
     local lsp_attach = function(client, bufnr)
@@ -140,10 +147,11 @@ if not vim.g.vscode then
         extensions = {},
     }
 
-    require("marks").setup()
-
-    require('oil').setup()
-    require('onedark').load()
+    require("peek").setup({
+        auto_load = false
+    })
+    vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+    vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
 
     require('telescope').setup({
         pickers = {
@@ -157,8 +165,6 @@ if not vim.g.vscode then
         direction = 'float'
     }
 
-    require("venv-selector").setup()
-
     require("nvim-treesitter.configs").setup{
         highlight = {
             enable = true,
@@ -166,6 +172,55 @@ if not vim.g.vscode then
         indent = {
             enable = true,
         },
+        textobjects = {
+            select = {
+                enable = true,
+                lookahead = true,
+                keymaps = {
+                    ["ia"] = "@parameter.inner",
+                    ["aa"] = "@parameter.outer",
+                    ["if"] = "@function.inner",
+                    ["af"] = "@function.outer",
+                    ["ic"] = "@class.inner",
+                    ["ac"] = "@class.outer",
+                    ["ii"] = "@conditional.inner",
+                    ["ai"] = "@conditional.outer",
+                    ["il"] = "@loop.inner",
+                    ["al"] = "@loop.outer",
+                    ["ik"] = "@block.inner",
+                    ["ak"] = "@block.outer",
+                    ["is"] = "@statement.inner",
+                    ["as"] = "@statement.outer",
+                    ["im"] = "@call.inner",
+                    ["am"] = "@call.outer",
+                },
+            },
+            move = {
+                enable = true,
+                set_jumps = true,
+                goto_next_start = {
+                    ["]f"] = "@function.outer",
+                },
+                goto_next_end = {
+                    ["]F"] = "@function.outer",
+                },
+                goto_previous_start = {
+                    ["[f"] = "@function.outer",
+                },
+                goto_previous_end = {
+                    ["[F"] = "@function.outer"
+                },
+            },
+            swap = {
+                enable = true,
+                swap_next = {
+                    [")a"] = "@parameter.inner",
+                },
+                swap_previous = {
+                    ["(a"] = "@parameter.inner",
+                }
+            },
+        }
     }
     -- Needed for Treesitter-supported folding
     vim.wo.foldmethod = 'expr'
@@ -216,23 +271,23 @@ filetype indent on
 --     ]])
 -- end
 
-function map(mode, shortcut, cmd)
+local function map(mode, shortcut, cmd)
     vim.api.nvim_set_keymap(mode, shortcut, cmd, { noremap = true, silent = true })
 end
 
-function nnomap(shortcut, cmd)
+local function nnomap(shortcut, cmd)
     map('n', shortcut, cmd)
 end
 
-function inomap(shortcut, cmd)
+local function inomap(shortcut, cmd)
     map('i', shortcut, cmd)
 end
 
-function vnomap(shortcut, cmd)
+local function vnomap(shortcut, cmd)
     map('v', shortcut, cmd)
 end
 
-function tnomap(shortcut, cmd)
+local function tnomap(shortcut, cmd)
     map('t', shortcut, cmd)
 end
 
@@ -341,7 +396,7 @@ if not vim.g.vscode then
     })
 
     -- Custom function to add toggle functionality to diffview.nvim
-    local last_tabpage = vim.api.nvim_get_current_tabpage()
+    -- local last_tabpage = vim.api.nvim_get_current_tabpage()
     function DiffviewToggle()
         local lib = require("diffview.lib")
         local view = lib.get_current_view()
